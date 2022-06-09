1276d35d4e43c94a66b42a367e751a2cff097671
DaoKhanhDuy <90609070+DaoKhanhDuy@users.noreply.github.com>
2022-06-09 09:08:51 +0700

    Create 20thembanbe.php

---
+++ 20thembanbe.php
@@ -0,0 +1,173 @@
+error_reporting(0);
+session_start();
+date_default_timezone_set('Asia/Ho_Chi_Minh');
+$res="\033[0m";
+$red="\033[1;31m";
+$white= "\033[1;37m";
+$whiteb= "\033[1;37m";
+$redb="\033[1;31m";
+$green="\033[1;32m";
+$yellow="\033[1;33m";
+$cam="\033[1;33m";
+$test="\033[1;33m";
+$greenb="\033[1;32m";
+$blue="\033[1;34m";
+$lam="\033[1;34m";
+$tmi="\033[1;34m";
+$hong="\033[1;35m";
+$imt="\033[1;35m";
+$cyan= "\e[1;36m";
+$syan="\033[1;36m";
+$xnhac= "\033[1;96m";
+$den="\033[1;90m";
+$do="\033[1;91m";
+$luc="\033[1;92m";
+$vang="\033[1;93m";
+$xduong="\033[1;94m";
+$hong="\033[1;95m";
+$trang="\033[1;97m";
+$vang="\033[1;93m";
+$do="\033[1;91m";
+$BBlack="\033[1;30m" ;      
+$BRed="\033[1;31m"         ;
+$BGreen="\033[1;32m"       ;
+$BYellow="\033[1;33m"   ;
+$BBlue="\033[1;34m"        ;
+$BPurple="\033[1;35m"      ;
+$BCyan="\033[1;36m"   ;
+$BWhite="\033[1;37m"    ;
+$Blue="\033[0;34m";
+$lime="\033[1;32m";
+$red="\033[1;31m";
+$xanh="\033[1;32m";
+$cyan="\033[1;36m";
+$yellow="\033[1;33m";
+$turquoise="\033[1;34m";
+$maugi="\033[1;35m";
+$white= "\033[1;37m";
+$BCyan="\033[1;36m";
+$trang="\033[1;37m";
+$do="\033[1;31m";
+$luc="\033[1;32m";
+$vang="\033[1;33m";
+$luc="\033[1;92m";
+$thuong = $do."[".$luc."●".$do."]".$trang." => ";
+$thanhngang = $vang."-------------------------------------------------------------\n";
+$weekday = date("l"); 
+$ngay = date("d"); 
+$thang = date("m"); 
+$nam = date("Y"); 
+$weekday = strtolower($weekday);switch($weekday) { case 'monday': $weekday = "\033[1;33mThứ\033[1;36m 2"; break; case 'tuesday': $weekday = "\033[1;33mThứ\033[1;36m 3"; break; case 'wednesday': $weekday = "\033[1;33mThứ \033[1;36m4"; break; case 'thursday': $weekday = "\033[1;33mThứ \033[1;36m5"; break; case 'friday': $weekday = "\033[1;33mThứ\033[1;36m 6"; break; case 'saturday': $weekday = "\033[1;33mThứ \033[1;36m7"; break; default: $weekday = "\033[1;33mCN"; break; }
+
+ ///key
+ //Key tool
+if (!$sock = @fsockopen('www.google.com', 80)) {echo 'Vui lòng bật kết nối mạng';
+exit;
+  }
+
+@system('clear');
+echo "\033[1;32m╔═══════════════════════════════════════════════════════════╗\n";
+usleep(70000);
+echo $thuong.$vang."Tool Thêm Bạn Đơn Cookie\n";
+usleep(70000);
+echo $thuong.$xnhac."Bản Quyền Thưởng Channel\n";
+usleep(70000);
+echo $thuong.$BBlue."Web Bán Xu + Key Tool + VPS:".$do." http://thuongchannel.xyz\n";
+usleep(70000);
+echo $thuong.$maugi."Web Tăng Like Follow:".$do." http://thuonglike.tk\n";
+usleep(70000);
+echo $thuong.$xnhac."Hôm Nay \033[1;36m$weekday \033[1;33mNgày \033[1;36m$ngay \033[1;33mTháng \033[1;36m$thang \033[1;33mNăm \033[1;36m$nam$end\033[0;00m\n";
+usleep(70000);
+echo $thuong.$hong."IP Tool Của Bạn: \033[1;97m$ip\n";
+usleep(70000);
+echo $luc."╚═══════════════════════════════════════════════════════════╝\n";
+usleep(70000); 
+echo "\033[1;31m● \033[1;32mNhập Cookie Facebook: \033[1;33m";
+    $cookie = trim(fgets(STDIN));
+system('clear');
+echo "\033[1;32m╔═══════════════════════════════════════════════════════════╗\n";
+usleep(70000);
+echo $thuong.$vang."Tool Thêm Bạn Đơn Cookie\n";
+usleep(70000);
+echo $thuong.$xnhac."Bản Quyền Thưởng Channel\n";
+usleep(70000);
+echo $thuong.$BBlue."Web Bán Xu + Key Tool + VPS:".$do." http://thuongchannel.xyz\n";
+usleep(70000);
+echo $thuong.$maugi."Web Tăng Like Follow:".$do." http://thuonglike.tk\n";
+usleep(70000);
+echo $thuong.$xnhac."Hôm Nay \033[1;36m$weekday \033[1;33mNgày \033[1;36m$ngay \033[1;33mTháng \033[1;36m$thang \033[1;33mNăm \033[1;36m$nam$end\033[0;00m\n";
+usleep(70000);
+echo $thuong.$hong."IP Tool Của Bạn: \033[1;97m$ip\n";
+usleep(70000);
+echo $luc."╚═══════════════════════════════════════════════════════════╝\n";
+usleep(70000); 
+    echo $thuong.$luc."Nhập Delay: $vang";
+    $a = trim(fgets(STDIN));
+    echo $thanhngang;
+    while (true) {
+        $url = "https://mbasic.facebook.com/";
+$head = array(
+            "Host: mbasic.facebook.com",
+            "upgrade-insecure-requests: 1",
+            "save-data: on",
+            "user-agent: Mozilla/5.0 (Linux; Android 5.1.1; SM-J320G) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Mobile Safari/537.36",
+            "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*" . "/" . "*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "sec-fetch-site: same-origin",
+            "sec-fetch-mode: navigate",
+            "sec-fetch-user: ?1",
+            "sec-fetch-dest: document",
+            "accept-language: vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5",
+            "cookie: $cookie",
+        );
+        $ch = curl_init();
+        curl_setopt_array($ch, array(
+            CURLOPT_URL => $url,
+            CURLOPT_FOLLOWLOCATION => false,
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_POST => 1,
+            CURLOPT_HTTPGET => true,
+            CURLOPT_SSL_VERIFYPEER => 0,
+            CURLOPT_HTTPHEADER => $head,
+            CURLOPT_HEADER => true,
+            CURLOPT_ENCODING => TRUE
+        ));
+        $c = curl_exec($ch);
+        $one = explode('href="/friends/center/mbasic/?', $c);
+        $two = explode('"', $one[1]);
+        $url = "https://mbasic.facebook.com/friends/center/?" . htmlspecialchars_decode($two[0]);
+        $ch = curl_init();
+        curl_setopt_array($ch, array(
+            CURLOPT_URL => $url,
+            CURLOPT_FOLLOWLOCATION => false,
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_POST => 1,
+            CURLOPT_HTTPGET => true,
+            CURLOPT_SSL_VERIFYPEER => 0,
+            CURLOPT_HTTPHEADER => $head,
+            CURLOPT_HEADER => true,
+            CURLOPT_ENCODING => TRUE
+        ));
+        $c = curl_exec($ch);
+        $one = explode('href="/a/mobile/friends/add_friend.php?', $c);
+        $kb = explode('"', $one[1]);
+        $url = "https://mbasic.facebook.com/a/mobile/friends/add_friend.php?" . htmlspecialchars_decode($kb[0]);
+        $ch = curl_init();
+        curl_setopt_array($ch, array(
+            CURLOPT_URL => $url,
+            CURLOPT_FOLLOWLOCATION => false,
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_POST => 1,
+            CURLOPT_HTTPGET => true,
+            CURLOPT_SSL_VERIFYPEER => 0,
+            CURLOPT_HTTPHEADER => $head,
+            CURLOPT_HEADER => true,
+            CURLOPT_ENCODING => TRUE
+        ));
+        $c = curl_exec($ch);
+        $id = explode('id=', $kb[0])[1];
+        $id = explode('&', $id)[0];
+echo $do."[".$luc."●".$do."] ".$xnhac.date("H:i:s").$do." ● ".$vang."Thêm Thành Công ID".$do." ● ".$luc.$id."\n";
+for ($i=$a;$i>-1;$i--){
+echo "● Đợi $i Giây \r";
+sleep(1);}
+}
